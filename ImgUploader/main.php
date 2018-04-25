44213d3fe842dd0316d5d2fb3e55754bf069dbbd
BadFoxer <31113326+BadFoxer@users.noreply.github.com>
2018-04-25 09:45:48 +0300

    Add files via upload

---
+++ ImgUploader/main.php
@@ -0,0 +1,13 @@
+<?php require 'uploaderis.php'; ?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="main.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="image" >
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>
